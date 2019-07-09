4ed68a6e7af033e3b67ba3f469fe72a717cf4ac3
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2019-07-09 20:04:45 +0200

    ..

---
+++ emonpi-3phase/src/src.ino
@@ -0,0 +1,5 @@
+/*  -*- coding: utf-8; mode: c; indent-tabs-mode: nil -*- */
+
+const int firmware_version = 10;             // The firmware version 1.0
+
+#include <LiquidCrystal_I2C.h>               // https://github.com/openenergymonitor/LiquidCrystal_I2C
