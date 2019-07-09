16cb718d092228fe76971198065fcc973da8306c
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2019-07-10 00:23:04 +0200

    ..

--- emonpi-3phase/src/src.ino
+++ emonpi-3phase/src/src.ino
@@ -1,5 +1,11 @@
 /*  -*- coding: utf-8; mode: c; indent-tabs-mode: nil -*- */
 
-const int firmware_version = 10;             // The firmware version 1.0
+// gcc -dM -E - < /dev/null
+// g++ -dM -E -x c++ - < /dev/null
+// -dD is similar to -dM but does not include predefined macros
 
-#include <LiquidCrystal_I2C.h>               // https://github.com/openenergymonitor/LiquidCrystal_I2C
+// echo "#include <stdlib.h>" | gcc -x c++ -std=c++11 -dD -E -
+
+const int firmware_version = 10;  // The firmware version 1.0
+
+#include <LiquidCrystal_I2C.h>  // https://github.com/openenergymonitor/LiquidCrystal_I2C
